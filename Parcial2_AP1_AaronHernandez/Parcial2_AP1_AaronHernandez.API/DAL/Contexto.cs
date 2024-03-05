ff197b0b19f1c570299aac815ebe32571dc3a8be
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-03-04 20:53:05 -0400

    Elaborando el contexto

--- Parcial2_AP1_AaronHernandez/Parcial2_AP1_AaronHernandez.API/DAL/Contexto.cs
+++ Parcial2_AP1_AaronHernandez/Parcial2_AP1_AaronHernandez.API/DAL/Contexto.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Compartido.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Parcial2_AP1_AaronHernandez.API.DAL
 {
 	public class Contexto : DbContext
 	{
+		public DbSet<Vehiculos>? Vehiculos { get; set; }
+		public DbSet<Accesorios>? Accesorios { get; set; }
+		public DbSet<VehiculoDetalle>? VehiculoDetalle { get; set; }
 
+		public Contexto(DbContextOptions<Contexto> options) : base(options){ }	
 	}
 }
