76d4511568d216cec0f6691550f2a23aab29cd19
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-03-04 20:52:50 -0400

    Realizando la migracion

---
+++ Parcial2_AP1_AaronHernandez/Parcial2_AP1_AaronHernandez.API/Migrations/20240304223727_Inicial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial2_AP1_AaronHernandez.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accesorios",
+                columns: table => new
+                {
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accesorios", x => x.AccesorioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculoDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculoDetalle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculos",
+                columns: table => new
+                {
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    Gasto = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculos", x => x.VehiculoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accesorios");
+
+            migrationBuilder.DropTable(
+                name: "VehiculoDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculos");
+        }
+    }
+}
