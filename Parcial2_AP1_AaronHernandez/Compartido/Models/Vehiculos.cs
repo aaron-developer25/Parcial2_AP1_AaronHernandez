948be422b43234f3ee405a442224a2a65d241799
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-03-04 20:51:55 -0400

    Creacion del modelo Vehiculo

---
+++ Parcial2_AP1_AaronHernandez/Compartido/Models/Vehiculos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compartido.Models
+{
+	public class Vehiculos
+	{
+		[Key]
+		public int VehiculoId { get; set; }
+
+		[Required(ErrorMessage ="Este campo es requerido")]
+		public string? Descripcion { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		public DateTime Fecha { get; set; }
+
+		[Required(ErrorMessage = "Este campo es requerido")]
+		[Range(0.01, float.MaxValue, ErrorMessage = "Debe ser mayor a 0.01")]
+		public float Costo { get; set;}
+		public float Gasto { get; set; }
+
+	}
+}
